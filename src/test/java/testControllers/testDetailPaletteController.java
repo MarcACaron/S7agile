e8d94a79ea91e6bb82b1e47988c992adbae9e3bf
Marc-Antoine <marc.ant.caron@gmail.com>
2020-01-30 11:49:27 -0500

    missing stuff

---
+++ src/test/java/testControllers/testDetailPaletteController.java
@@ -0,0 +1,40 @@
+package testControllers;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import view.DetailPaletteController;
+
+
+public class testDetailPaletteController {
+	private DetailPaletteController detailPaletteController;
+	/*
+	@Test
+	public void paletteDisable_test() {
+		detailPaletteController = new DetailPaletteController();
+		
+		detailPaletteController.paletteDisable(true);
+		assertEquals(detailPaletteController.getPalette().isDisabled(), true);
+		
+		detailPaletteController.paletteDisable(false);
+		assertEquals(detailPaletteController.getPalette().isDisabled(), false);
+	}
+	
+	@Test
+	public void select_test() {
+		detailPaletteController.select(false, false, false, false, false);
+		assertEquals(detailPaletteController.getXPosText(), false);
+		assertEquals(detailPaletteController.getRadiusText(), false);
+		assertEquals(detailPaletteController.getWidthText(), false);
+		assertEquals(detailPaletteController.getHeightText(), false);
+		assertEquals(detailPaletteController.getAngleText(), false);
+		
+		detailPaletteController.select(true, true, true, true, true);
+		assertEquals(detailPaletteController.getXPosText(), true);
+		assertEquals(detailPaletteController.getRadiusText(), true);
+		assertEquals(detailPaletteController.getWidthText(), true);
+		assertEquals(detailPaletteController.getHeightText(), true);
+		assertEquals(detailPaletteController.getAngleText(), true);		
+	}*/
+}
