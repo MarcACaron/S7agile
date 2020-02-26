4af7db13d9801ca1aba9ecbaf10084eaa5152033
Pitis <gpitis@hotmail.fr>
2020-02-26 11:03:47 -0500

    Correction double import

--- src/test/java/testsFX/testPaletteFormController.java
+++ src/test/java/testsFX/testPaletteFormController.java
@@ -6,8 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javafx.scene.input.MouseButton;
-
 
 public class testPaletteFormController extends testApplicationUI {
 	@BeforeEach
